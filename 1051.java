ab659f04c7018afe4848766b79a0371c31674ec4
李雨阳 <liyuyang@rius-MacBook-Pro.local>
2019-09-05 22:22:09 -0400

    initial commit

---
+++ 1051.java
@@ -0,0 +1,14 @@
+/// Arrays.copyOf is the copy of the array with input int[] & length
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] copy = Arrays.copyOf(heights, heights.length);
+        Arrays.sort(heights);
+        int count=0;
+        for (int i=0; i<heights.length; i++){
+            if (heights[i]!=copy[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
